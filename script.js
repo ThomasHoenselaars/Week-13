700750cf56e61a5bb4e03c5efdaa76c02be70974
Thomas Hoenselaars <heemsteda@gmail.com>
2019-02-05 13:12:24 +0100

    HAMBURGER TRANSITION WERKT

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const dropdownMenu = document.querySelector('.nav');
 const dropdownNavList = document.querySelector('.nav-list');
 const dropdownLogo = document.querySelector('.logo');
-const dropdownHamburger = document.querySelector('.hamburger');
+const hamburgerChange = document.querySelector('.hamburger .fas');
+
+console.log(hamburgerChange);
+
 const dropdownRotated = document.querySelector('.nav-item.rotated');
 
 
@@ -12,6 +15,6 @@ menuButton.addEventListener('click', function(){
     dropdownMenu.classList.toggle('foldout');
     dropdownNavList.classList.toggle('foldout');
     dropdownLogo.classList.toggle('foldout');
-    dropdownHamburger.classList.toggle('foldout');
     dropdownRotated.classList.toggle('foldout');
+    hamburgerChange.classList.toggle('fa-times');
 });
